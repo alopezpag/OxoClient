a1fc50cbe12722116bedff7d4461e2afbd244a67
Arnau López <108700238+alopezpag@users.noreply.github.com>
2023-11-29 11:56:27 +0100

    Created Main Classes and added MC source code for vanilla

---
+++ src/main/java/net/oxo/OxoClient.java
@@ -0,0 +1,10 @@
+package net.oxo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OxoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
