28ecc631867bf692bcbc9fe71cfc8abebac6bb6f
Arnau López <108700238+alopezpag@users.noreply.github.com>
2023-11-29 11:42:22 +0100

    Initial commit

---
+++ src/main/java/net/oxo/OxoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.oxo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OxoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
